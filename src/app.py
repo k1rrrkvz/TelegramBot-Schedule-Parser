83d8586ee0d106b14e79e258c20dd8b74ab1974d
K1rrrkvz <126238623+k1rrrkvz@users.noreply.github.com>
2023-08-09 19:30:18 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,19 +1,6 @@
-import asyncio
-import sched, time
 from aiogram import executor
 from handlers import dp
-from chrome_driver.main import runParser 
-
-"""def def_runParser():
-    while True:
-        os.system('python путь_к_файлу.py')
-        print("Парсер запущен")"""
-
 
 if __name__ == "__main__":
-    #asyncio
     print("Бот запущен!")
     executor.start_polling(dp, skip_updates = True)     # все ранее отправленные сообщения боту, когда он был выключен, будут пропущены
-    
-
-
